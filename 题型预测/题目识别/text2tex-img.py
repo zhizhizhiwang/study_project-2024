8e67b5ec9d43570c9251978fecfcd43628e9a8f9
zhizhizhiwang <1073171253@qq.com>
2024-06-23 14:04:24 +0800

    small update

--- 题型预测/题目识别/text2tex-img.py
+++ 题型预测/题目识别/text2tex-img.py
@@ -1,26 +1,24 @@
 from PIL import Image
 from pix2tex.cli import LatexOCR
 
-
 img = Image.open(r'./img/text1-tex.jpg')
 
-
-
+# 先转成灰度图
 im = img.convert("L")
 
-threshold  = 150
-table  =  []
-for  i  in  range( 256 ):
-     if  i  <  threshold:
+threshold = 150
+table = []
+for i in range(256):
+    if i < threshold:
         table.append(0)
-     else :
+    else:
         table.append(1)
- #  convert to binary image by the table 
-bim  =  im.point(table,"1" )
+#  convert to binary image by the table
+# 灰度图根据table转换为二值图
+bim = im.point(table, "1")
 bim.show()
 
 # new_img = img.point(lambda x : 0 if x < 200 else 1, "1")
 # new_img.show()
 model = LatexOCR()
 print(model(bim))
-
